308ea3099700746d6b5060248f925761bbd42c23
Daniel <daniel@tohka.us>
2023-03-14 01:13:45 +0300

    fix linting in contract controller

--- controllers/contracts/contractsController.ts
+++ controllers/contracts/contractsController.ts
@@ -2,27 +2,27 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { createContract } from './createContract';
 
 export const newContract = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { friendlyName, description } = req.body;
+  const { friendlyName, description } = req.body;
 
-    try {
-      const contract = await createContract({ friendlyName, description });
-      return res.json({
-        ok: true,
-        contract: contract.toJSON()
-      });
-    } catch (err) {
-      if (err.name === 'ContractCreationError') {
-        return res.status(400).json({
-          ok: false,
-          error: err.code
-        });
-      }
-
-      console.error(err);
-
-      return res.status(500).json({
+  try {
+    const contract = await createContract({ friendlyName, description });
+    return res.json({
+      ok: true,
+      contract: contract.toJSON()
+    });
+  } catch (err) {
+    if (err.name === 'ContractCreationError') {
+      return res.status(400).json({
         ok: false,
-        error: 'INTERNAL_SERVER_ERROR'
+        error: err.code
       });
     }
-  };
+
+    console.error(err);
+
+    return res.status(500).json({
+      ok: false,
+      error: 'INTERNAL_SERVER_ERROR'
+    });
+  }
+};
