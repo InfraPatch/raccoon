382f3bc7ee2fe2a1ce6bb8314a3bbbe43f20d98e
József Sallai <jozsef@sallai.me>
2023-03-15 19:34:11 +0300

    add filled contract API service

--- services/apis/contracts/FilledContractAPIService.ts
+++ services/apis/contracts/FilledContractAPIService.ts
@@ -1,4 +1,4 @@
-import { APIResponse } from '@/services/axios';
+import axiosService, { APIResponse } from '@/services/axios';
 import { IFilledContract } from '@/db/models/contracts/FilledContract';
 
 export interface NewFilledContractAPIParams {
@@ -20,3 +20,74 @@ export interface ListFillContractsAPIResponse extends APIResponse {
   own: IFilledContract[];
   foreign: IFilledContract[];
 };
+
+export interface CreateFilledContractAPIResponse extends APIResponse {
+  filledContract: IFilledContract;
+};
+
+export interface GetFilledContractAPIResponse extends APIResponse {
+  filledContract: IFilledContract;
+};
+
+export interface FillFilledContractAPIResponse extends APIResponse {};
+export interface DeleteFilledContractAPIResponse extends APIResponse {};
+
+export interface AcceptFilledContractAPIResponse extends APIResponse {};
+export interface DeclineFilledContractAPIResponse extends APIResponse {};
+
+export interface SignFilledContractAPIResponse extends APIResponse {};
+
+export class FilledContractAPIService {
+  static LIST_FILLED_CONTRACTS_URL = '/api/filled-contracts';
+  static CREATE_FILLED_CONTRACT_URL = '/api/filled-contracts';
+
+  static GET_FILLED_CONTRACT_URL = '/api/filled-contract/:id';
+  static FILL_FILLED_CONTRACT_URL = '/api/filled-contract/:id';
+  static DELETE_FILLED_CONTRACT_URL = '/api/filled-contract/:id';
+
+  static DOWNLOAD_FILLED_CONTRACT_URL = '/api/filled-contract/:id/download';
+
+  static ACCEPT_FILLED_CONTRACT_URL = '/api/filled-contract/:id/accept';
+  static DECLINE_FILLED_CONTRACT_URL = '/api/filled-contract/:id/decline';
+
+  static SIGN_FILLED_CONTRACT_URL = '/api/filled-contract/:id/sign';
+
+  private url(template: string, id: number) {
+    return template.replace(':id', id.toString());
+  }
+
+  public async listFilledContracts(): Promise<ListFillContractsAPIResponse> {
+    return axiosService.get(FilledContractAPIService.LIST_FILLED_CONTRACTS_URL)
+      .then(res => res.data);
+  }
+
+  public async createFilledContract(payload: NewFilledContractAPIParams): Promise<CreateFilledContractAPIResponse> {
+    return axiosService.post(FilledContractAPIService.CREATE_FILLED_CONTRACT_URL, payload)
+      .then(res => res.data);
+  }
+
+  public async fillFilledContract(id: number, payload: FillContractOptionAPIParams): Promise<GetFilledContractAPIResponse> {
+    return axiosService.put(this.url(FilledContractAPIService.GET_FILLED_CONTRACT_URL, id), payload)
+      .then(res => res.data);
+  }
+
+  public async deleteFilledContract(id: number): Promise<DeleteFilledContractAPIResponse> {
+    return axiosService.delete(this.url(FilledContractAPIService.DELETE_FILLED_CONTRACT_URL, id))
+      .then(res => res.data);
+  }
+
+  public async acceptFilledContract(id: number): Promise<AcceptFilledContractAPIResponse> {
+    return axiosService.patch(this.url(FilledContractAPIService.ACCEPT_FILLED_CONTRACT_URL, id))
+      .then(res => res.data);
+  }
+
+  public async declineFilledContract(id: number): Promise<DeclineFilledContractAPIResponse> {
+    return axiosService.patch(this.url(FilledContractAPIService.DECLINE_FILLED_CONTRACT_URL, id))
+      .then(res => res.data);
+  }
+
+  public async signFilledContract(id: number): Promise<SignFilledContractAPIResponse> {
+    return axiosService.patch(this.url(FilledContractAPIService.SIGN_FILLED_CONTRACT_URL, id))
+      .then(res => res.data);
+  }
+}
