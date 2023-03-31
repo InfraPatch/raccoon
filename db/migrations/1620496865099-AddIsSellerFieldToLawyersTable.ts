06f7b3c9c39e116fbac6a6404e21be5109404edf
József Sallai <jozsef@sallai.me>
2023-03-31 21:02:10 +0300

    migration for adding isSeller to lawyers table

---
+++ db/migrations/1620496865099-AddIsSellerFieldToLawyersTable.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddIsSellerFieldToLawyersTable1620496865099 implements MigrationInterface {
+    name = 'AddIsSellerFieldToLawyersTable1620496865099'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `witnessSignatures` ADD `isSeller` tinyint NOT NULL");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `witnessSignatures` DROP COLUMN `isSeller`");
+    }
+
+}
