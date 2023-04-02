ed7fa9b0299e893345585db9a4e8df983d9a6c7e
József Sallai <jozsef@sallai.me>
2023-04-02 20:30:58 +0300

    add /dashboard/inventory route

--- pages/dashboard/inventory/index.tsx
+++ pages/dashboard/inventory/index.tsx
@@ -1,7 +1,94 @@
-const DashboardItemsPage = () => {
+import { useEffect, useState } from 'react';
+
+import { GetServerSideProps } from 'next';
+import DashboardLayout from '@/layouts/DashboardLayout';
+
+import { User } from '@/db/models/auth/User';
+import Columns from '@/components/common/columns/Columns';
+import Column from '@/components/common/columns/Column';
+
+import { getSession } from 'next-auth/client';
+import { redirectIfAnonymous } from '@/lib/redirects';
+
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import apiService from '@/services/apis';
+
+import { Item } from '@/db/models/items/Item';
+import ItemBox from '@/components/dashboard/items/ItemBox';
+import { useTranslation } from 'react-i18next';
+import Meta from '@/components/common/Meta';
+import Loading from '@/components/common/Loading';
+import { DangerMessage } from '@/components/common/message-box/DangerMessage';
+
+export interface DashboardNewContractPageProps {
+  user: User;
+};
+
+const DashboardItemsPage = ({ user }: DashboardNewContractPageProps) => {
+  const { t } = useTranslation([ 'dashboard', 'errors' ]);
+
+  const [ items, setItems ] = useState<Item[] | null>(null);
+  const [ error, setError ] = useState('');
+
+  const loadItems = async () => {
+    setItems(null);
+    setError('');
+
+    try {
+      const res = await apiService.items.getItems();
+      setItems(res.items)
+    } catch (err) {
+      console.error(err);
+      setError(t('errors:INTERNAL_SERVER_ERROR'));
+    }
+  };
+
+  useEffect(() => {
+    loadItems();
+  }, []);
+
   return (
-    <p>This page is a work in progress.</p>
+    <DashboardLayout user={user}>
+      <Meta
+        title={ t('dashboard:pages.my-items') }
+        url="/dashboard/inventory"
+      />
+
+      <h1 className="font-normal text-2xl mb-6">
+        { t('dashboard:pages.my-items') }
+      </h1>
+
+      <Columns>
+        <Column>
+          {items && items.map((item, idx) => (
+            <ItemBox item={item} key={idx} />
+          ))}
+        </Column>
+
+        <Column />
+      </Columns>
+
+      {!items && !error && <Loading />}
+
+      {error && error.length && <DangerMessage>{error}</DangerMessage>}
+    </DashboardLayout>
   );
+};
+
+export const getServerSideProps : GetServerSideProps = async ({ req, res, locale }) => {
+  const session = await getSession({ req });
+
+  if (await redirectIfAnonymous(res, session)) {
+    return { props: { user: null } };
+  }
+
+  return {
+    props: {
+      user: session.user,
+      ...await serverSideTranslations(locale, [ 'common', 'dashboard', 'errors' ])
+    }
+  };
 };
 
 export default DashboardItemsPage;
