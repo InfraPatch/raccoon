39913c92be42bc3b939ed0a1d2ee3502e82c093c
Daniel <daniel@tohka.us>
2023-04-02 12:16:04 +0300

    add search to contract page

--- pages/dashboard/contracts/index.tsx
+++ pages/dashboard/contracts/index.tsx
@@ -31,19 +31,48 @@ export interface DashboardContractListProps {
   partyType: PartyType;
 };
 
+export interface DashboardContractTypeProps {
+  id: number;
+  friendlyName: string;
+};
+
 const DashboardContractListPage = ({ user }: DashboardContractListPageProps) => {
   const { t } = useTranslation([ 'dashboard', 'errors' ]);
+  const unsortedContractType = { id: -1, friendlyName: t('dashboard:contracts.list.all-contracts') };
 
   const [ ownContracts, setOwnContracts ] = useState<IFilledContract[] | null>(null);
   const [ foreignContracts, setForeignContracts ] = useState<IFilledContract[] | null>(null);
   const [ witnessContracts, setWitnessContracts ] = useState<IFilledContract[] | null>(null);
+  const [ contractTypes, setContractTypes ] = useState<DashboardContractTypeProps[]>([ unsortedContractType ]);
+
+  // Search arguments
+  const [ filteredName, setFilteredName ] = useState<string | null>(null);
+  const [ filteredType, setFilteredType ] = useState<number | null>(null);
 
   const [ error, setError ] = useState('');
 
   const loadContracts = async () => {
+    const collectContractTypes = (contracts: IFilledContract[], contractTypes : DashboardContractTypeProps[]) => {
+      if (!contracts) {
+        return;
+      }
+
+      for (const filledContract of contracts) {
+        const contract = filledContract.contract;
+
+        // Add this contract type if we haven't encountered it previously
+        if (!contractTypes.some(t => t.id === contract.id)) {
+          contractTypes.push({ id: contract.id, friendlyName: contract.friendlyName });
+        }
+      }
+    }
+
     setOwnContracts(null);
     setForeignContracts(null);
     setWitnessContracts(null);
+    setFilteredName(null);
+    setFilteredType(null);
+    setContractTypes([ unsortedContractType ]);
 
     setError('');
 
@@ -53,6 +82,19 @@ const DashboardContractListPage = ({ user }: DashboardContractListPageProps) => 
       setOwnContracts(res.own);
       setForeignContracts(res.foreign);
       setWitnessContracts(res.witness);
+
+      const types : DashboardContractTypeProps[] = [];
+      collectContractTypes(res.own, types);
+      collectContractTypes(res.foreign, types);
+      collectContractTypes(res.witness, types);
+
+      // Sort types by name
+      types.sort((a, b) => a.friendlyName.localeCompare(b.friendlyName));
+
+      // Add default, unsorted type
+      types.unshift(unsortedContractType);
+
+      setContractTypes(types);
     } catch (err) {
       console.error(err);
       setError(t('errors:INTERNAL_SERVER_ERROR'));
@@ -85,10 +127,43 @@ const DashboardContractListPage = ({ user }: DashboardContractListPageProps) => 
     );
   };
 
+  const filterContracts = (contracts: IFilledContract[]) => {
+    if (contracts === null) {
+      return null;
+    }
+
+    return contracts.filter(c => {
+      if (filteredName &&
+          !c.friendlyName.trim().toLowerCase().includes(filteredName) &&
+          !c.contract.friendlyName.trim().toLowerCase().includes(filteredName) &&
+          !c.buyer?.name?.trim().toLowerCase().includes(filteredName) &&
+          !c.user?.name?.trim().toLowerCase().includes(filteredName)
+      ) {
+        return false;
+      }
+
+      if (filteredType && c.contract.id !== filteredType) {
+        return false;
+      }
+
+      return true;
+    });
+  }
+
+  const searchForName = (e) => {
+    const name = e.target.value.trim().toLowerCase();
+    setFilteredName(name.length !== 0 ? name : null);
+  };
+
+  const searchForType = (e) => {
+    const type = Number(e.target.value);
+    setFilteredType(type !== -1 ? type : null);
+  };
+
   let columns : DashboardContractListProps[] = [
-    {contracts: ownContracts, type: 'own', partyType: PartyType.SELLER},
-    {contracts: foreignContracts, type: 'buyer', partyType: PartyType.BUYER},
-    {contracts: witnessContracts, type: 'witness', partyType: PartyType.WITNESS}
+    {contracts: filterContracts(ownContracts), type: 'own', partyType: PartyType.SELLER},
+    {contracts: filterContracts(foreignContracts), type: 'buyer', partyType: PartyType.BUYER},
+    {contracts: filterContracts(witnessContracts), type: 'witness', partyType: PartyType.WITNESS}
   ];
 
   // Sort the columns based on whether they are empty or not
@@ -107,11 +182,29 @@ const DashboardContractListPage = ({ user }: DashboardContractListPageProps) => 
       {!error && (
         <Columns>
           <Column>
+            <Box>
+              <div className="text-center">
+                <div className="form-field">
+                  <label htmlFor="searchName">{ t('dashboard:contracts.list.search-by-name') }</label>
+                  <input id="searchName" name="searchName" type="text" onChange={searchForName} placeholder={ t('dashboard:contracts.list.contract-name') } />
+                </div>
+              </div>
+            </Box>
             {contractLists[0]}
             {contractLists[2]}
           </Column>
 
           <Column>
+            <Box>
+              <div className="text-center">
+                <div className="form-field">
+                  <label htmlFor="searchType">{ t('dashboard:contracts.list.search-by-type') }</label>
+                  <select id="searchType" name="searchType" onChange={searchForType}>
+                    {contractTypes.map(t => <option key={t.id} value={t.id}>{ t.friendlyName }</option>)}
+                  </select>
+                </div>
+              </div>
+            </Box>
             {contractLists[1]}
           </Column>
         </Columns>
