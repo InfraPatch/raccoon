d599dae21da9c45984be9f8f1d337bf03d405368
József Sallai <jozsef@sallai.me>
2023-04-04 09:17:15 +0300

    add nprogress axios interceptor

--- services/axios.ts
+++ services/axios.ts
@@ -1,5 +1,7 @@
 import buildUrl from '@/lib/buildUrl';
+
 import { AxiosRequestConfig, AxiosResponse, default as axios } from 'axios';
+import NProgress from 'nprogress';
 
 export interface APIResponse {
   ok: boolean;
@@ -11,6 +13,34 @@ export interface APIErrorResponse extends APIResponse {
     [key: string]: any;
   };
 };
+
+axios.interceptors.request.use(req => {
+  if (typeof window !== 'undefined' && ![ 'get', 'GET' ].includes(req.method)) {
+    NProgress.start();
+  }
+
+  return req;
+}, err => {
+  if (typeof window !== 'undefined') {
+    NProgress.done();
+  }
+
+  return Promise.reject(err);
+});
+
+axios.interceptors.response.use(res => {
+  if (typeof window !== 'undefined') {
+    NProgress.done();
+  }
+
+  return res;
+}, err => {
+  if (typeof window !== 'undefined') {
+    NProgress.done();
+  }
+
+  return Promise.reject(err);
+});
 
 class AxiosService {
   public async get<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
